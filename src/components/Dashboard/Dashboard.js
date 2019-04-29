2085c0b07cac4ab14d4f8112bb51b87fe36d55ac
Adrian Gruber <Adrian_Gruber@epam.com>
2019-04-29 08:34:28 +0200

    Finish Hero Search basic functionality

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Dashboard.module.scss';
 import Cards from './Cards/Cards';
 import HeroSearch from './HeroSearch/HeroSearch';
 
-const Dashboard = () => {
-  const topHeroes = ['Batman', 'Flash', 'Superman', 'Green lantern', 'Aquaman'];
+class Dashboard extends Component {
+  state = {
+    heroes: [
+      'Batman',
+      'Flash',
+      'Superman',
+      'Green lantern',
+      'Aquaman',
+      'Spider-Man'
+    ],
+    filteredHeroes: [],
+    inputValue: ''
+  };
 
-  return (
-    <div className={classes.Dashboard}>
-      <h2 className={classes.TopHeroesCaption}>TOP HEROES</h2>
-      <Cards heroes={topHeroes} />
-      <h3>Hero Search</h3>
-      <HeroSearch heroes={topHeroes} />
-    </div>
-  );
-};
+  filterHeroHandler = event => {
+    let inputValue = event.target.value;
+    let filteredHeroes;
+
+    if (inputValue) {
+      filteredHeroes = this.state.heroes.filter(heroName =>
+        heroName.toLowerCase().includes(inputValue.toLowerCase())
+      );
+    }
+
+    this.setState({
+      inputValue: inputValue,
+      filteredHeroes: filteredHeroes
+    });
+  };
+
+  render() {
+    return (
+      <div className={classes.Dashboard}>
+        <h2 className={classes.TopHeroesCaption}>TOP HEROES</h2>
+        <Cards heroes={this.state.heroes} />
+        <h3>Hero Search</h3>
+        <HeroSearch
+          filteredHeroes={this.state.filteredHeroes}
+          changed={this.filterHeroHandler}
+          value={this.state.inputValue}
+        />
+      </div>
+    );
+  }
+}
 
 export default Dashboard;
--- src/components/Dashboard/HeroSearch/HeroSearch.js
+++ src/components/Dashboard/HeroSearch/HeroSearch.js
@@ -1,9 +1,37 @@
 import React from 'react';
 
-const HeroSearch = props => {
-  const heroes = props.heroes;
+import classes from './HeroSearch.module.scss';
 
-  return <input type="text" />;
+const HeroSearch = props => {
+  const heroesNames = props.filteredHeroes;
+  let searchItems;
+
+  if (heroesNames) {
+    searchItems = heroesNames.map((heroName, index) => (
+      <div key={index} className={classes.SearchResultItem}>
+        {heroName}
+      </div>
+    ));
+  }
+
+  return (
+    <React.Fragment>
+      <input
+        type="text"
+        onChange={event => props.changed(event)}
+        value={props.value}
+      />
+      <div
+        className={
+          heroesNames && heroesNames.length !== 0
+            ? classes.SearchResultContainer
+            : null
+        }
+      >
+        {searchItems}
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default HeroSearch;
