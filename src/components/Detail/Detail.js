4498bb50673c75b14f845c780940dcf3f41aa14b
adrian.gruber <gruber950513@gmail.com>
2019-05-06 19:07:33 +0200

    Add Datail page template with static data

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import classes from './Detail.module.scss';
+
 const Detail = () => {
-  return <div>Detail</div>;
+  return (
+    <div className={classes.Detail}>
+      <h2>
+        Hero Detail: <span className={classes.HeroName}>Aquaman</span>
+      </h2>
+      <p>id: 11</p>
+      <input type="text" />
+      <div className={classes.ButtonContainer}>
+        <button>Go back</button>
+        <button>Save</button>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
